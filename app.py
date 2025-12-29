ae27333266b9ef13f47f6a0036e3948dc51a209a
sakethreddynalla <sakethreddynalla96@gmail.com>
2025-12-29 15:45:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+
 import requests
+import mysql.connector
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_mysqldb import MySQL
 
 load_dotenv()
-
 API_KEY = os.getenv("NINJA_API_KEY")
+con = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE")
+    )
+cursor = con.cursor()
 
 if not API_KEY:
     raise ValueError("API key not found. Check your .env file.")
@@ -29,14 +38,35 @@ def generate_ai_quote():
     else:
         return "No quote received", "Unknown"
 
-@app.route("/")
-def home():
+@app.route("/ai")
+def index():
     quote, author = generate_ai_quote()
     return render_template(
         "index.html",
         quote=quote,
         author=author
     )
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/registration', methods=['GET','POST'])
+def registration():
+    if request.method == "POST":
+        user = request.form["username"]
+        email = request.form['email']
+        password = request.form['password']
+        cursor.execute("insert into registration(username,email,password) values (%s,%s,%s)",[user,email,password])
+        con.commit()
+        return "values stored"
+    return render_template('registration.html') 
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
